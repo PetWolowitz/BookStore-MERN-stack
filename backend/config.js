b827bf59ee581a3b774d4daf4a95388f4bf4087f
PetWolowitz <156096630+PetWolowitz@users.noreply.github.com>
2024-06-21 18:15:08 +0200

    MongoDB aggiunto al progetto

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,5 @@
 export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://root:root@books-store-mern.4ycscbn.mongodb.net/?retryWrites=true&w=majority&appName=Books-store-MERN"
+
+// please create a free database for yourself
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+app.get("/", (request, response) => {
+    console.log("request")
+    return response.status(234).send('Welcome to my APP')
 })
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("connected to mongoDB");
+        app.listen(PORT, () => {
+            console.log(`listening on port ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
