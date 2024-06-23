a02f63b5f4356612068a104d2c424cec48a79d0f
PetWolowitz <156096630+PetWolowitz@users.noreply.github.com>
2024-06-23 18:12:10 +0200

    Frontend impostato

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,24 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import bookRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 // Middleware for parsing request body
 app.use(express.json());
+
+// middleware for cors
+// app.use(cors());
+app.use(
+    cors({
+        origin: "http://localhost:5555",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        allowedHeaders: ["Content-Type"]
+        
+    })
+)
+
+
 app.get("/", (request, response) => {
     console.log("request")
     return response.status(234).send('Welcome to my APP')
